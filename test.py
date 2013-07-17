7d296b9ab6a6e611f242f6bf81c10777cf9dbd72
Chris Merck <cmerck@merck-ubuntu.(none)>
2013-07-17 12:03:47 -0400

    Added mel filters.

--- test.py
+++ test.py
@@ -5,6 +5,11 @@ import struct
 import matplotlib.pyplot as plt
 from pylab import *
 
+'''
+REFERENCES:
+	www.practicalcryptography.com - MFCC Guide
+	'''
+
 rate = 44100
 
 def plot_wf(wf):
@@ -38,17 +43,83 @@ def split_frame(frame, nsamp, overlap):
 		yield frame[head:head+nsamp]
 		i += 1
 
-raw = read_frame(int(0.5*rate))
-raw = read_frame(1*rate)
+def freq_to_mels(freq):
+	return 1125 * np.log(1 + freq/700.)
+
+def mels_to_freq(mels):
+	return 700 * (np.exp(mels/1125.) - 1)
+
+def get_spectrum_x_axis(frame_len):
+	return [ i * rate / float(frame_len) for i in range(frame_len)]
+
+def make_mel_filterbank_funcs(fmin,fmax,size):
+	melmin = freq_to_mels(fmin)
+	melmax = freq_to_mels(fmax)
+	melpts = np.linspace(melmin,melmax,size+2)
+	peakfreqs = mels_to_freq(melpts)
+	filter_funcs = []
+	for i in range(size):
+		def filter_func(freq,i=i):
+			''' triangular mel filter '''
+			if freq < peakfreqs[i]:
+				return 0
+			elif freq < peakfreqs[i+1]:
+				return (freq - peakfreqs[i])/(peakfreqs[i+1]-peakfreqs[i])
+			elif freq < peakfreqs[i+2]:
+				return (peakfreqs[i+2]-freq)/(peakfreqs[i+2]-peakfreqs[i+1])
+			else:
+				return 0
+		filter_funcs += [filter_func]
+	return filter_funcs
+
+def render_filterbank(filter_funcs, X):
+	''' X = list of x-axis values (frequencies) at which
+	  to evaludate the filter functions '''
+	return [ [ filter_func(x) for x in X ] for filter_func in filter_funcs]
+
+def filterbank_energies(filterbank,powerspec):
+	fbe = []
+	for f in filterbank:
+		e = np.sum(np.multiply(f,powerspec))
+		fbe += [e]
+	return fbe
+
+
+#raw = read_frame(int(0.5*rate))
+raw = read_frame(2*rate)
+
+fbf = make_mel_filterbank_funcs(400,10000,26)
+fb = None
 
 ps = []
+fbes = []
 
 for subframe in split_frame(raw, int(.025*rate), int(.015*rate)):
 	w = window(subframe)
 	write_frame(w)
+
+	p = log_power_spectrum(w)
+	ps += [p] #[50:200]] # save for plotting
+
+	if not fb:
+		fb = render_filterbank(fbf, get_spectrum_x_axis(len(p)))
+		'''
+		for f in fb:
+			plot(f)
+			show()
+			'''
 	
-	ps += [log_power_spectrum(w)[0:200]]
+	fbe = filterbank_energies(fb,p)
+	#print >> sys.stderr, "FBE=", fbe
+	fbes += [fbe]
+
+
+
 
 ps = np.transpose(ps)
+ax1 = subplot(211)
 imshow(ps,origin="lower")
+subplot(212,sharex=ax1)
+imshow(np.transpose(fbes),origin="lower")
+#yticks(range(ps.shape[0]), get_spectrum_x_axis(ps.shape[1]))
 show()
